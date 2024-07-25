ff7b30b3c490e0f8593cc5a6b4aeb4f6e1c85e1d
ViNiCiM0 <vinic.mach05@gmail.com>
2024-07-25 11:45:07 -0300

    Criação do método excluir videos.

--- novovideo.php
+++ novovideo.php
@@ -17,8 +17,8 @@ if ($titulo === false){
 
 $sql = 'INSERT INTO videos (url, title) VALUES(?, ?)';
 $stmt = $pdo->prepare($sql);
-$stmt->bindValue(1, $_POST['url']);
-$stmt->bindValue(2, $_POST['titulo']);
+$stmt->bindValue(1,$url);
+$stmt->bindValue(2,$titulo);
 
 if ($stmt->execute() === false){
     header('Location: index.php?sucesso=0');
